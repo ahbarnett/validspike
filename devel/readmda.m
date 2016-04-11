89f3959fffe7b09544304aa6ca74b66c618a2205
ahbarnett <ahb@math.dartmouth.edu>
2016-04-11 16:28:31 -0400

    updated write/readmda to avoid conflict with mountainlab*

--- devel/readmda.m
+++ devel/readmda.m
@@ -1,37 +1,62 @@
 function A=readmda(fname)
+%READMDA - read the contents of a .mda file. MDA stands for
+%multi-dimensional array.
+%
+% See http://magland.github.io//articles/mda-format/
+%
+% Syntax: A=readmda(fname)
+%
+% Inputs:
+%    fname - path to the .mda file
+%
+% Outputs:
+%    A - the multi-dimensional array
+%
+% Other m-files required: none
+%
+% See also: writemda
+
+% Author: Jeremy Magland
+% Jan 2015; Last revision: 15-Feb-2106
 
 F=fopen(fname,'rb');
 
+try
 code=fread(F,1,'long');
-if (code>0) %this is the rare (old) case of complex numbers
+catch
+    error('Problem reading file: %s',fname);
+end
+if (code>0) 
     num_dims=code;
     code=-1;
 else
-    fread(F,1,'long'); % number of bytes per data entry, we skip it
+    fread(F,1,'long');
     num_dims=fread(F,1,'long');    
 end;
 
 S=zeros(1,num_dims);
 for j=1:num_dims
-    S(j)=fread(F,1,'long'); %Size of each dimension
+    S(j)=fread(F,1,'long');
 end;
 N=prod(S);
 
 A=zeros(S);
 if (code==-1)
     M=zeros(1,N*2);
-    M(:)=fread(F,N*2,'float32');
-    A(:)=M(1:2:prod(S)*2)+i*M(2:2:prod(S)*2); %Split into real/imag
+    M(:)=fread(F,N*2,'float');
+    A(:)=M(1:2:prod(S)*2)+i*M(2:2:prod(S)*2);
 elseif (code==-2)
     A(:)=fread(F,N,'uchar');
 elseif (code==-3)
-    A(:)=fread(F,N,'float32');
+    A(:)=fread(F,N,'float');
 elseif (code==-4)
-    A(:)=fread(F,N,'int16');
+    A(:)=fread(F,N,'short');
 elseif (code==-5)
     A(:)=fread(F,N,'int32');
 elseif (code==-6)
     A(:)=fread(F,N,'uint16');
+elseif (code==-7)
+    A(:)=fread(F,N,'double');
 end;
 
 fclose(F);
--- utils/writemda.m
+++ utils/writemda.m
@@ -1,20 +1,50 @@
-function writemda(X,fname)
-% WRITEMDA - write to JFM's multi-dimensional array file format
+function writemda(X,fname,dtype)
+%WRITEMDA - write to a .mda file. MDA stands for
+%multi-dimensional array.
 %
-% writemda(X,fname) writes the array X to the file fname. X can have any number
-%  of dimensions up to 6.
+% See http://magland.github.io//articles/mda-format/
+%
+% Syntax: writemda(X,fname)
+%
+% Inputs:
+%    X - the multi-dimensional array
+%    fname - path to the output .mda file
+%    dtype - 'complex32', 'int32', 'float32','float64'
+%
+% Other m-files required: none
+%
+% See also: readmda
 
-% JFM's. todo: finish documentation
-
+% Author: Jeremy Magland
+% Jan 2015; Last revision: 15-Feb-2016; typo fixed Barnett 2/26/16
 num_dims=2;
-if (size(X,3)>1) num_dims=3; end;
-if (size(X,4)>1) num_dims=4; end;
-if (size(X,5)>1) num_dims=5; end;
-if (size(X,6)>1) num_dims=6; end;
+if (size(X,3)~=1) num_dims=3; end; % ~=1 added by jfm on 11/5/2015 to handle case of, eg, 10x10x0
+if (size(X,4)~=1) num_dims=4; end;
+if (size(X,5)~=1) num_dims=5; end;
+if (size(X,6)~=1) num_dims=6; end;
+
+if nargin<3, dtype=''; end;
+
+if isempty(dtype)
+    %warning('Please use writemda32 or writemda64 rather than directly calling writemda. This way you have control on whether the file stores 32-bit or 64-bit floating points.');
+    is_complex=1;
+    if (isreal(X)) is_complex=0; end;
+
+    if (is_complex)
+        dtype='complex32';
+    else
+        is_integer=check_if_integer(X);
+        if (~is_integer)
+            dtype='float32';
+        else
+            dtype='int32';
+        end;
+    end;
+end;
+
 FF=fopen(fname,'w');
-complex=1;
-if (isreal(X)) complex=0; end;
-if (complex)
+
+if strcmp(dtype,'complex32')
     fwrite(FF,-1,'int32');
     fwrite(FF,8,'int32');
     fwrite(FF,num_dims,'int32');
@@ -28,7 +58,7 @@ if (complex)
     Y(1:2:dimprod*2-1)=real(XS);
     Y(2:2:dimprod*2)=imag(XS);
     fwrite(FF,Y,'float32');
-else
+elseif strcmp(dtype,'float32')
     fwrite(FF,-3,'int32');
     fwrite(FF,4,'int32');
     fwrite(FF,num_dims,'int32');
@@ -39,6 +69,38 @@ else
     end;
     Y=reshape(X,dimprod,1);
     fwrite(FF,Y,'float32');
+elseif strcmp(dtype,'float64')
+    fwrite(FF,-7,'int32');
+    fwrite(FF,8,'int32');
+    fwrite(FF,num_dims,'int32');
+    dimprod=1;
+    for dd=1:num_dims
+        fwrite(FF,size(X,dd),'int32');
+        dimprod=dimprod*size(X,dd);
+    end;
+    Y=reshape(X,dimprod,1);
+    fwrite(FF,Y,'float64');
+elseif strcmp(dtype,'int32')
+    fwrite(FF,-5,'int32');
+    fwrite(FF,4,'int32');
+    fwrite(FF,num_dims,'int32');
+    dimprod=1;
+    for dd=1:num_dims
+        fwrite(FF,size(X,dd),'int32');
+        dimprod=dimprod*size(X,dd);
+    end;
+    Y=reshape(X,dimprod,1);
+    fwrite(FF,Y,'int32');
+else
+    error('Unknown dtype %s',dtype);
 end;
 fclose(FF);
+end
+
+function ret=check_if_integer(X)
+ret=0;
+if (length(X)==0) ret=1; return; end;
+if (X(1)~=round(X(1))) ret=0; return; end;
+tmp=X(:)-round(X(:));
+if (max(abs(tmp))==0) ret=1; end;
 end
